7f8c74ecf0f2d18edda1ec214eae3ba9a8167ccb
YoucefKhalainou <youcef_khalainou@yahoo.fr>
2021-03-31 13:14:43 +0200

    Config Gmail

--- config/environment.rb
+++ config/environment.rb
@@ -5,18 +5,10 @@ require_relative "application"
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-
-    :address => 'smtp.sendgrid.net',
-
-    :port => '587',
-
-    :authentication => :plain,
-
-    :user_name => ENV['SENDGRID_USERNAME'],
-
-    :password => ENV['SENDGRID_PASSWORD'],
-
-    :domain => 'heroku.com',
-             
-    :enable_starttls_auto => true
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'gmail.com', #you can also use google.com
+    :authentication     => :plain,
+    :user_name          => ENV['GMAIL_LOGIN'],
+    :password           => ENV['GMAIL_PWD']
 }
